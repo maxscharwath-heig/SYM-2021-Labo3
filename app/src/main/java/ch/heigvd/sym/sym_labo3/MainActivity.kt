327fc8520f96f5620cd792a677baa599a482487f
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-11-23 09:31:37 +0100

    Main activity

--- app/src/main/java/ch/heigvd/sym/sym_labo3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/MainActivity.kt
@@ -1,11 +1,43 @@
 package ch.heigvd.sym.sym_labo3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var barcodeActivityBtn : Button
+    private lateinit var beaconActivityBtn : Button
+    private lateinit var NFCActivityBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        barcodeActivityBtn = findViewById(R.id.btn_qr_launch)
+        beaconActivityBtn = findViewById(R.id.btn_beacon_launch)
+        NFCActivityBtn = findViewById(R.id.btn_nfc_launch)
+
+        barcodeActivityBtn.setOnClickListener {
+            val intent = Intent(this, QRActivity::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
+
+        beaconActivityBtn.setOnClickListener {
+            val intent = Intent(this, BeaconActivity::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
+
+        NFCActivityBtn.setOnClickListener {
+            val intent = Intent(this, NFCActivity::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
     }
 }
