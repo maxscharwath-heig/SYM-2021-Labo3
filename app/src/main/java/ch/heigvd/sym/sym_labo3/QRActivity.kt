306595340efde0a48ea74f7c8bfc1933e9df6c50
nicrausaz <n.crausaz99@gmail.com>
2021-11-30 09:44:57 +0100

    Qr scan + display text & image

--- app/src/main/java/ch/heigvd/sym/sym_labo3/QRActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/QRActivity.kt
@@ -1,28 +1,38 @@
 package ch.heigvd.sym.sym_labo3
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.journeyapps.barcodescanner.ScanOptions
 import android.widget.Toast
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
-
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
 class QRActivity : AppCompatActivity() {
 
     private lateinit var scanBtn : Button
+    private lateinit var scanImage: ImageView
+    private lateinit var scanText: TextView
+    private lateinit var barcodeView: DecoratedBarcodeView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // TODO: Need to embed the scanner in the activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr)
 
         scanBtn = findViewById(R.id.scan_btn)
+        scanImage = findViewById(R.id.barcodeResultImg)
+        scanText = findViewById(R.id.barcodeNumber)
+        // barcodeView = findViewById(R.id.barcodeCompound)
 
         scanBtn.setOnClickListener {
             val options = ScanOptions()
             options.setOrientationLocked(false)
+            options.setBarcodeImageEnabled(true)
             barcodeLauncher.launch(options)
             return@setOnClickListener
         }
@@ -33,11 +43,9 @@ class QRActivity : AppCompatActivity() {
         if (result.contents == null) {
             Toast.makeText(this@QRActivity, "Cancelled", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(
-                this@QRActivity,
-                "Scanned: " + result.contents,
-                Toast.LENGTH_LONG
-            ).show()
+            scanText.text = result.contents
+            val bmImg = BitmapFactory.decodeFile(result.barcodeImagePath)
+            scanImage.setImageBitmap(bmImg)
         }
     }
 }
