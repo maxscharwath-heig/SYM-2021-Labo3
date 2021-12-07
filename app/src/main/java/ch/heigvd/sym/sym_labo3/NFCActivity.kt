667bd60e7e22e1a03d4457dab1532a0fa490132d
Maxime Scharwath <maxscharwath@gmail.com>
2021-12-07 08:59:24 +0100

    got nfc

--- app/src/main/java/ch/heigvd/sym/sym_labo3/NFCActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/NFCActivity.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.app.PendingIntent
+import android.content.ContentValues.TAG
 import android.content.IntentFilter
 import android.nfc.NfcAdapter.ACTION_TECH_DISCOVERED
 import android.nfc.Tag
 import android.nfc.tech.IsoDep
+import android.util.Log
 
 
 class NFCActivity : AppCompatActivity() {
@@ -18,10 +20,6 @@ class NFCActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc)
-    }
-
-    override fun onResume() {
-        super.onResume()
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
             Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
@@ -36,18 +34,40 @@ class NFCActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
-        val launchIntent = Intent(this, this.javaClass)
-        launchIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, launchIntent, PendingIntent.FLAG_CANCEL_CURRENT
-        )
+    }
 
-        val filters = arrayOf(IntentFilter(ACTION_TECH_DISCOVERED))
-        val techTypes = arrayOf(arrayOf(IsoDep::class.java.name))
+    private fun setupForegroundDispatch() {
+        if (nfcAdapter == null) return
+        val intent = Intent(this.applicationContext, this.javaClass)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(this.applicationContext, 0, intent, 0)
+        val filters = arrayOfNulls<IntentFilter>(1)
+        val techList = arrayOf<Array<String>>()
+        // On souhaite être notifié uniquement pour les TAG au format NDEF
+        filters[0] = IntentFilter()
+        filters[0]!!.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        filters[0]!!.addCategory(Intent.CATEGORY_DEFAULT)
+        try {
+            filters[0]!!.addDataType("text/plain")
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            Log.e(TAG, "MalformedMimeTypeException", e)
+        }
+        nfcAdapter!!.enableForegroundDispatch(this, pendingIntent, filters, techList)
+    }
 
-        nfcAdapter!!.enableForegroundDispatch(
-            this, pendingIntent, filters, techTypes
-        )
+    // called in onPause()
+    private fun stopForegroundDispatch() {
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopForegroundDispatch();
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -55,11 +75,5 @@ class NFCActivity : AppCompatActivity() {
         var action = intent?.action
         if(action == null) action = "nothing"
         Toast.makeText(this, action, Toast.LENGTH_SHORT).show()
-        if (NfcAdapter.ACTION_TECH_DISCOVERED == action) {
-            val tag = intent?.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-            IsoDep.get(tag)?.let { isoDepTag ->
-                Toast.makeText(this,isoDepTag.tag.id.toString(),Toast.LENGTH_SHORT)
-            }
-        }
     }
 }
