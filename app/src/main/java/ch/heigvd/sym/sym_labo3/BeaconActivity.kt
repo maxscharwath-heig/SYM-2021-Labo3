4f7756968e25a61d60d68622bea8e653afcb0a7b
LordTT <ferraricorneloup.teo@gmail.com>
2021-12-07 10:41:53 +0100

    Ibeacon working

--- app/src/main/java/ch/heigvd/sym/sym_labo3/BeaconActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/BeaconActivity.kt
@@ -9,14 +9,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.altbeacon.beacon.*
-
-
-
+import java.time.LocalDateTime
+import kotlin.math.round
 
 
 class BeaconActivity : AppCompatActivity() {
+
+    val beaconList = ArrayList<Beacon>()
+    val beaconArrayAdapter = BeaconAdapter(beaconList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,39 +40,72 @@ class BeaconActivity : AppCompatActivity() {
         // observer will be called each time the monitored regionState changes (inside vs. outside region)
         beaconManager.getRegionViewModel(region).rangedBeacons.observe(this, rangingObserver)
         beaconManager.startRangingBeacons(region)
+
+
+        beaconsView.layoutManager = LinearLayoutManager(this)
+        beaconsView.itemAnimator = DefaultItemAnimator()
+        beaconsView.adapter = beaconArrayAdapter
     }
 
     val rangingObserver = Observer<Collection<Beacon>> { beacons ->
         Log.d(TAG, "Ranged: ${beacons.count()} beacons")
         for (beacon: Beacon in beacons) {
             Log.d(TAG, "$beacon about ${beacon.distance} meters away")
+            if (beaconList.contains(beacon)) {
+                beaconList[beaconList.indexOf(beacon)] = beacon
+                beaconArrayAdapter.notifyItemChanged(beaconList.indexOf(beacon))
+            } else {
+                beaconList.add(beacon)
+                beaconArrayAdapter.notifyItemInserted(beaconList.indexOf(beacon))
+            }
         }
     }
 
-    // Create the basic adapter extending from RecyclerView.Adapter
-// Note that we specify the custom ViewHolder which gives us access to our views
-    class beaconAdapter : RecyclerView.Adapter<beaconAdapter.ViewHolder>() {
+    class BeaconAdapter(private val beaconList: List<Beacon>) :
+        RecyclerView.Adapter<BeaconAdapter.ViewHolder>() {
 
-        // Provide a direct reference to each of the views within a data item
-        // Used to cache the views within the item layout for fast access
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            // Your holder should contain and initialize a member variable
-            // for any view that will be set as you render a row
-            val majorTextView = itemView.findViewById<TextView>(R.id.major)
-        }
-
+        // create new views
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            // inflates the card_view_design view
+            // that is used to hold list item
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.beacon, parent, false)
-
             return ViewHolder(view)
         }
 
+        // binds the list items to a view
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+            val beacon = beaconList[position]
+            val time = LocalDateTime.now()
+            // sets the text to the textview from our itemHolder class
+            holder.idTextView.text = "Id : " + (position + 1)
+            holder.uuidTextView.text = "UUID : " + beacon.id1
+            holder.majorTextView.text = "major : " + beacon.id2
+            holder.minorTextView.text = "minor : " + beacon.id3
+            holder.rssiTextView.text = "rssi : " + beacon.rssi
+            holder.distanceTextView.text =
+                "distance : " + String.format("%.3f", beacon.distance) + " m"
+            holder.dateTextView.text = "last detection time : " +
+                    time.dayOfMonth + "-" + time.monthValue + "-" + time.year + "  " +
+                    time.hour + ":" + time.minute + ":" + time.second
         }
 
+        // return the number of the items in the list
         override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+            return beaconList.size
+        }
+
+        // Holds the views for adding it to image and text
+        class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+            val idTextView: TextView = itemView.findViewById(R.id.id)
+            val uuidTextView: TextView = itemView.findViewById(R.id.UUID)
+            val majorTextView: TextView = itemView.findViewById(R.id.major)
+            val minorTextView: TextView = itemView.findViewById(R.id.minor)
+            val rssiTextView: TextView = itemView.findViewById(R.id.rssi)
+            val distanceTextView: TextView = itemView.findViewById(R.id.distance)
+            val dateTextView: TextView = itemView.findViewById(R.id.date)
+
         }
     }
 }
