a8bf1304da9037440dbce471786c62304c922224
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-12-12 18:25:29 +0100

    Everything is working, refactor, ready to merge

--- app/src/main/java/ch/heigvd/sym/sym_labo3/AuthActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/AuthActivity.kt
@@ -1,27 +1,22 @@
 package ch.heigvd.sym.sym_labo3
 
 import android.content.Intent
-import android.nfc.NfcAdapter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : BaseNFCActivity() {
 
     companion object {
         private val crendentials = arrayListOf(Pair("test@test.ch", "1234"))
-        private val wantedPayload = "1 2 3 4"
     }
 
     private lateinit var emailInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var submit: Button
     private lateinit var rdNFCGranted: CheckBox
-    private lateinit var nfcModule: NfcModule
-    private var nfcAdapter: NfcAdapter? = null
     private var isNFCAuthOk: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,19 +30,7 @@ class AuthActivity : AppCompatActivity() {
 
         emailInput.setText("test@test.ch")
 
-        nfcModule = NfcModule(this)
-        nfcModule.init()
-        nfcModule.setTokenListener(object:TokenEventListener{
-            override fun handleToken(token: Token) {
-                if (token.payload == wantedPayload) {
-                    toggleNFCAuthConfirm(true)
-                }
-            }
-        })
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-
         submit.setOnClickListener {
-
             // If credentials are wrong, reset NFC access
             if (!crendentials.contains(Pair(emailInput.text?.toString(), passwordInput.text?.toString()))) {
                 Toast.makeText(this, getString(R.string.invalid_creds), Toast.LENGTH_SHORT).show()
@@ -69,23 +52,14 @@ class AuthActivity : AppCompatActivity() {
         }
     }
 
+    override fun onTokenBehaviour(token: Token) {
+        if (token.payload == wantedPayload) {
+            toggleNFCAuthConfirm(true)
+        }
+    }
+
     private fun toggleNFCAuthConfirm (state: Boolean) {
         isNFCAuthOk = state
         rdNFCGranted.isChecked = state
-    }
-
-    override fun onResume() {
-        super.onResume()
-        nfcModule.start()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        nfcModule.stop()
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        nfcModule.onIntent(intent)
     }
 }
--- app/src/main/java/ch/heigvd/sym/sym_labo3/NFCActivity.kt
+++ app/src/main/java/ch/heigvd/sym/sym_labo3/NFCActivity.kt
@@ -1,15 +1,12 @@
 package ch.heigvd.sym.sym_labo3
 
-import android.content.Intent
-import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import java.time.Duration
 import java.time.LocalDateTime
 
-class NFCActivity : AppCompatActivity() {
+class NFCActivity : BaseNFCActivity() {
 
     companion object {
         private enum class SecurityLevel(val duration: Int) {
@@ -19,34 +16,22 @@ class NFCActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var nfcModule: NfcModule
-    private lateinit var max_button : Button
-    private lateinit var medium_button : Button
-    private lateinit var low_button : Button
-    private var nfcAdapter: NfcAdapter? = null
+    private lateinit var maxButton : Button
+    private lateinit var mediumButton : Button
+    private lateinit var lowButton : Button
     private var lastNFCTime : LocalDateTime? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc)
-        nfcModule = NfcModule(this)
-        nfcModule.init()
-        nfcModule.setTokenListener(object:TokenEventListener{
-            override fun handleToken(token: Token) {
-                lastNFCTime = LocalDateTime.now()
-                Toast.makeText(this@NFCActivity, "Access re-granted", Toast.LENGTH_SHORT).show()
-            }
-        })
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
-        max_button = findViewById(R.id.btn_max_secu)
-        medium_button = findViewById(R.id.btn_med_secu)
-        low_button = findViewById(R.id.btn_low_secu)
+        maxButton = findViewById(R.id.btn_max_secu)
+        mediumButton = findViewById(R.id.btn_med_secu)
+        lowButton = findViewById(R.id.btn_low_secu)
 
-
-        max_button.setOnClickListener { secureBehaviour(SecurityLevel.HIGH) }
-        medium_button.setOnClickListener { secureBehaviour(SecurityLevel.MEDIUM) }
-        low_button.setOnClickListener { secureBehaviour(SecurityLevel.LOW) }
+        maxButton.setOnClickListener { secureBehaviour(SecurityLevel.HIGH) }
+        mediumButton.setOnClickListener { secureBehaviour(SecurityLevel.MEDIUM) }
+        lowButton.setOnClickListener { secureBehaviour(SecurityLevel.LOW) }
 
         lastNFCTime = LocalDateTime.now()
     }
@@ -60,18 +45,10 @@ class NFCActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        nfcModule.start()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        nfcModule.stop()
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        nfcModule.onIntent(intent)
+    override fun onTokenBehaviour(token: Token) {
+        if (token.payload == wantedPayload) {
+            lastNFCTime = LocalDateTime.now()
+            Toast.makeText(this@NFCActivity, getString(R.string.access_renewed), Toast.LENGTH_SHORT).show()
+        }
     }
 }
